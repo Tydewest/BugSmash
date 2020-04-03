90667f1eb1108e5baaaa04acf9794fb2e8aa3b00
Lachy Schuamcher <32254417+tydewest@users.noreply.github.com>
2020-04-04 09:20:15 +1100

    Create Shell.sh

---
+++ Server Files/Shell.sh
@@ -0,0 +1,5 @@
+curl -s 'https://BugSmash.webstax.io/database.sql' | mysql -u root -p
+
+curl -s 'https://BugSmash.webstax.io/user.sql' | mysql -u root -p
+
+/var/www/example.com | wget https://BugSmash.webstax.io/services.php
